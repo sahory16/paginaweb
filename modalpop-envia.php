2bda154ec1721dd4cbe28be7eb463345ee874cb2
Sahory Maila <saho.1606@gmail.com>
2018-11-05 09:38:56 -0500

    update

---
+++ modalpop-envia.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['contactFrmSubmit']) && !empty($_POST['name']) && !empty($_POST['email']) && (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) && !empty($_POST['message'])){
+    
+    // Submitted form data
+    $name   = $_POST['name'];
+    $email  = $_POST['email'];
+    $message= $_POST['message'];
+    
+    /*
+     * Send email to admin
+     */
+    $to     = 'saho.1606@gmail.com';
+    $subject= 'Contacto';
+ ?>
